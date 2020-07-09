34a5dd816510137369938175b76ac48832b02716
Frank-L93 <frank.lambregts@outlook.com>
2020-07-09 21:46:39 +0200

    error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
 @section('title', __('Not Found'))
-@section('code', '404')
+@section('code', 'OEPS!')
 @section('message', __('Deze pagina bestaat niet. Ga terug!'))
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
 @section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('OEPS! Ga maar terug waar je vandaan kwam. Want hier heb je niks te zoeken. Wilde je iets doen en ging dat niet? Zeg dat dan even tegen Frank'))
+@section('code', 'OEPS!')
+@section('message', __('Ga maar terug waar je vandaan kwam. Want hier heb je niks te zoeken. Wilde je iets doen en ging dat niet? Zeg dat dan even tegen Frank'))
