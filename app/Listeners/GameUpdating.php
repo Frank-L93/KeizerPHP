f394fc885af46470107b47985783a009b23ca510
Frank-L93 <frank.lambregts@outlook.com>
2022-01-28 23:13:15 +0100

    Notifications version 1 af

--- app/Listeners/GameUpdating.php
+++ app/Listeners/GameUpdating.php
@@ -28,68 +28,71 @@ class GameUpdating
      */
     public function handle($event)
     {
+        if ($event->game->wasChanged('result')) {
+            $text_White = match ($event->game->result) {
+                '1-0' => 'gewonnen',
+                '0.5-0.5' => 'remise gespeeld',
+                '0-1' => 'verloren',
+                'Afwezigheid' => 'Afwezigheid',
+                default => 'nog geen resultaat',
+            };
+            $text_Black = match ($event->game->result) {
+                '1-0' => 'verloren',
+                '0.5-0.5' => 'remise gespeeld',
+                '0-1' => 'gewonnen',
+                default => 'nog geen resultaat',
+            };
 
-        $text_White = match ($event->game->result) {
-            '1-0' => 'gewonnen',
-            '0.5-0.5' => 'remise gespeeld',
-            '0-1' => 'verloren',
-            'Afwezigheid' => 'Afwezigheid',
-            default => 'nog geen resultaat',
-        };
-        $text_Black = match ($event->game->result) {
-            '1-0' => 'verloren',
-            '0.5-0.5' => 'remise gespeeld',
-            '0-1' => 'gewonnen',
-            default => 'nog geen resultaat',
-        };
+            $round = Round::find($event->game->round_id);
+            $white = $event->game->white;
+            $user_white = User::find($white);
 
-        $round = Round::find($event->game->round_id);
-        $white = $event->game->white;
-        $user_white = User::find($white);
+            $black = 0;
+            $user_black = 2;
+            if ($event->game->black == "Bye" || $event->game->result == "Afwezigheid") {
+            } else {
+                $black = $event->game->black;
+            }
+            if ($black !== 0) {
+                $user_black = User::find($black);
+                if ($user_black->hasSettings->notifications == true) {
+                    // Check what kind of notifications are necessary
+                    if ($user_black->hasSettings->NotifyByMail == true) {
+                        // Sent Notification Per Mail
 
-        $black = 0;
-        $user_black = 2;
-        if ($event->game->black == "Bye" || $event->game->result == "Afwezigheid") {
-        } else {
-            $black = $event->game->black;
-        }
-        if ($black !== 0) {
-            $user_black = User::find($black);
-            if ($user_black->hasSettings->notifications == true) {
+                        $user_black->notify(new ScoreNotification($event, $round, $text_Black, $black, $user_white, "Mail"));
+                    }
+                    if ($user_black->hasSettings->NotifyByDB == true) {
+                        $user_black->notify(new ScoreNotification($event, $round, $text_Black, $black, $user_white, "Database"));
+                    }
+                    if ($user_black->hasSettings->NotifyByRSS == true) {
+                        // Sent Notification Per RSS
+                        //$user_black->notify(new GameNotification($event, $round, $black, $user_white, "RSS"));
+                    }
+                }
+            }
+
+            if ($user_white->hasSettings->notifications == true) {
+
                 // Check what kind of notifications are necessary
-                if ($user_black->hasSettings->NotifyByMail == true) {
+                if ($user_white->hasSettings->NotifyByMail == true) {
+
                     // Sent Notification Per Mail
-
-                    $user_black->notify(new ScoreNotification($event, $round, $text_Black, $black, $user_white, "Mail"));
+                    $user_white->notify(new ScoreNotification($event, $round, $text_White, $black, $user_black, "Mail"));
                 }
-                if ($user_black->hasSettings->NotifyByDB == true) {
-                    $user_black->notify(new ScoreNotification($event, $round, $text_Black, $black, $user_white, "Database"));
+                if ($user_white->hasSettings->NotifyByDB == true) {
+                    $user_white->notify(new ScoreNotification($event, $round, $text_White, $black, $user_black, "Database"));
                 }
-                if ($user_black->hasSettings->NotifyByRSS == true) {
+                if ($user_white->hasSettings->NotifyByRSS == true) {
                     // Sent Notification Per RSS
-                    //$user_black->notify(new GameNotification($event, $round, $black, $user_white, "RSS"));
+                    // $user_white->notify(new GameNotification($event, $round, $black, $user_black, "RSS"));
                 }
             }
+
+
+            app('log')->info($event->game);
+        } else {
+            // Result is not changed, don't send notification.
         }
-
-        if ($user_white->hasSettings->notifications == true) {
-
-            // Check what kind of notifications are necessary
-            if ($user_white->hasSettings->NotifyByMail == true) {
-
-                // Sent Notification Per Mail
-                $user_white->notify(new ScoreNotification($event, $round, $text_White, $black, $user_black, "Mail"));
-            }
-            if ($user_white->hasSettings->NotifyByDB == true) {
-                $user_white->notify(new ScoreNotification($event, $round, $text_White, $black, $user_black, "Database"));
-            }
-            if ($user_white->hasSettings->NotifyByRSS == true) {
-                // Sent Notification Per RSS
-                // $user_white->notify(new GameNotification($event, $round, $black, $user_black, "RSS"));
-            }
-        }
-
-
-        app('log')->info($event->game);
     }
 }
