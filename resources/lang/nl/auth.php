ccbc6bee948c8f4467f07de3eb066565b713d246
Frank-L93 <frank.lambregts@outlook.com>
2022-03-10 20:48:44 +0100

    Release 2 werk

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'De ingevulde gegevens komen niet voor in onze database, controleer je ingevuld wachtwoord.',
+    'throttle' => 'Teveel mislukte inlogpogingen. Probeer opnieuw over :seconds secondes.',
+    'email' => 'E-mailadres',
+    'password' => 'Wachtwoord',
+    'header' => 'Login',
+    'remember' => 'Onthoudt mij',
+    'signin' => 'Log In',
+    'forgotten' => 'Je wachtwoord vergeten?',
+    'passwordChange' => 'Je wachtwoord is succesvol aangepast!',
+    'invalidCurrentPassword' => 'Je huidige wachtwoord is anders. Er kon dus geen nieuw wachtwoord aan je account gekoppeld worden!',
+    'emailChange' => 'Je e-mailadres is succesvol aangepast!',
+    'emailChangeFail' => 'Helaas konden wij je e-mailadres niet aanpassen',
+    'passwordLength' => 'Gebruik tenminste 8 karakters voor je wachtwoord',
+    'SameCurrentPassword' => 'Je moet wel een ander wachtwoord invullen natuurlijk!',
+    'SameNewPassword' => 'Bevestig je nieuwe wachtwoord!',
+];
